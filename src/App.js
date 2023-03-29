d120a058619debb6598578d9041cb6d9301083e8
MY PC <suryasrivastava724@gmail.com>
2023-03-29 10:35:45 +0530

    added small app using useContext hook

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './component/Login';
+import Status from './component/Status';
+
+function App() {
+  return (
+    <div className="App">
+      <Login/>
+      <Status/>
+    </div>
+  );
+}
+
+export default App;
